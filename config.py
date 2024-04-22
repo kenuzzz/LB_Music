2f6b9a3484e5e427f2bd27523498a9970b35d1ef
kenuzzz <167679827+kenuzzz@users.noreply.github.com>
2024-04-22 20:02:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -77,7 +77,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/b490638fd73cd0ec1d1ad.jpg"
+    "START_IMG_URL", "https://graph.org/file/6fbed54ace3c9552d559f.mp4"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org/file/b490638fd73cd0ec1d1ad.jpg"
